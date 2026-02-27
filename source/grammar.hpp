4f211772cc0d552733fe37de9671ce806a444d52
Tate Maguire <tatemaguir@gmail.com>
2026-02-26 23:00:53 -0800

    finished grammar class and its tests

--- source/grammar.hpp
+++ source/grammar.hpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <random>
 #include <iostream>
+#include <string>
 
 /////////////////////////////////////
 // Class Declaration
@@ -25,10 +26,16 @@ public:
     Grammar(std::initializer_list<Entry> init);
 
     void set_seed(std::random_device::result_type seed);
+    std::random_device::result_type get_seed();
     void addEntry(T value, int weight);
 
     T operator()();
+
+    std::string to_string() const;
 };
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const Grammar<T>& gram);
 
 /////////////////////////////////////
 // Function Definitions
@@ -38,6 +45,7 @@ template<class T>
 Grammar<T>::Grammar() {
     std::random_device rd;
     seed = rd();
+    re.seed(seed);
 }
 
 template<class T>
@@ -50,6 +58,12 @@ Grammar<T>::Grammar(std::initializer_list<Entry> init):
 template<class T>
 void Grammar<T>::set_seed(std::random_device::result_type num) {
     seed = num;
+    re.seed(seed);
+}
+
+template<class T>
+std::random_device::result_type Grammar<T>::get_seed() {
+    return seed;
 }
 
 template<class T>
@@ -58,3 +72,39 @@ void Grammar<T>::addEntry(T value, int weight) {
     entries.push_back(e);
 }
 
+template<class T>
+T Grammar<T>::operator()() {
+    int sum = 0;
+    for (const Entry& e : entries) {
+        sum += e.weight;
+    }
+
+    std::uniform_int_distribution<int> dist{1, sum};
+    int choice = dist(re);
+
+    T result;
+    int currentSum = 0;
+    for (const Entry& e : entries) {
+        currentSum += e.weight;
+        if (currentSum >= choice) {
+            result = e.value;
+            break;
+        }
+    }
+
+    return result;
+}
+
+template<class T>
+std::string Grammar<T>::to_string() const {
+    std::string result = "Size: " + std::to_string(entries.size()) + "\n";
+    for (const Entry& e : entries) {
+        result += "\tVal: " + std::to_string(e.value) + "\tWeight: " + std::to_string(e.weight) + "\n";
+    }
+    return result;
+}
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const Grammar<T>& gram) {
+    return os << gram.to_string();
+}
--- test/grammarTest.cpp
+++ test/grammarTest.cpp
@@ -1,10 +1,26 @@
+#include <iostream>
+#include <vector>
+
 #include "../source/grammar.hpp"
 
 int main(int argc, char* argv[]) {
 
-    Grammar<float> A;
-    A.addEntry(4.5, 3);
-    A.addEntry(3.9, 12);
+    Grammar<size_t> A;
+    A.addEntry(0, 10);
+    A.addEntry(1, 1);
+    A.addEntry(2, 10);
+
+    std::cout << A << std::endl;
+    std::vector<int> results(3);
+
+    for (int i = 0; i < 20; i++) {
+        results[A()]++;
+    }
+
+    std::cout << "results:" << std::endl;
+    for (int i = 0; i < 3; i++) {
+        std::cout << i << ": " << results[i] << std::endl;
+    }
 
     return 0;
 }
